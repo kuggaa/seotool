59d90e27303d860238bc4527ae665ad395c99d27
Aidan Brake <aidanbrake@gmail.com>
2014-10-01 21:46:18 +1300

    sd

--- Services/NATIVERANK/DB.php
+++ Services/NATIVERANK/DB.php
@@ -1,4 +1,4 @@
 <?php
-// $mysql = new mysqli("localhost", "root", "ajeRTIyzi8", "seotool");
+$mysql = new mysqli("localhost", "root", "ajeRTIyzi8", "seotool");
 
-$mysql = new mysqli("localhost", "root", "", "SEOTool");
+// $mysql = new mysqli("localhost", "root", "", "SEOTool");
