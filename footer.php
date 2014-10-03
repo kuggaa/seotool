f834807e069e814abb520cdd0b8faa438a65cedf
Aidan Brake <aidanbrake@gmail.com>
2014-10-03 15:00:23 +1300

    footer issue fixed.

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<footer class="full-w">
+<footer class="full-w" style="<?php if (count($allstat) == 0) echo 'position: fixed  !important;'; ?>">
 	<div class="cont-w">
 		<p class="copy">Copyright © 2014 NATIVE RANK. All rights reserved.</p>
 	</div>
