4463bf1682209cb10d95718f825fa358780fc6d3
Aidan Brake <aidanbrake@gmail.com>
2014-10-06 04:14:20 +1300

    sdf

--- config/config.php
+++ config/config.php
@@ -1,14 +1,24 @@
 <?php
-	/**
-	 *	DB Settings.
-	 */
-	$databaseHost		= "localhost";
-	$databaseUsername	= "root";
-	$databasePassword	= "";
-	$databaseName 		= "SEOTool";
 
 	/**
 	 *	Environment settins.
 	 */
 	$ENV = "dev";
+
+	/**
+	 *	DB Settings.
+	 */
+
+	if ($ENV == "dev")
+	{
+		$databaseHost		= "localhost";
+		$databaseUsername	= "root";
+		$databasePassword	= "";
+		$databaseName 		= "SEOTool";
+	} else {
+		$databaseHost		= "localhost";
+		$databaseUsername	= "root";
+		$databasePassword	= "ajeRTIyzi8";
+		$databaseName 		= "seotool";
+	}
 ?>
